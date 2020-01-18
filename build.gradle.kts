52d81d5518ec6f1f0a565e8ccf8e32451b8971f0
Viktor Kuzmin <kvaster@gmail.com>
2020-01-18 13:38:05 +0300

    java 11 is enough to run iptv-proxy, but java 13 is recommended

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_13
-    targetCompatibility = JavaVersion.VERSION_13
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 application {
