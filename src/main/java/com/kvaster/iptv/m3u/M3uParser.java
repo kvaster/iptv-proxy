fb7c28f9daca83983851320c833df2da81d8b598
Viktor Kuzmin <kvaster@gmail.com>
2021-04-02 00:52:40 +0300

    Sometimes we have malformed m3u data - values are provided without qoutes

--- src/main/java/com/kvaster/iptv/m3u/M3uParser.java
+++ src/main/java/com/kvaster/iptv/m3u/M3uParser.java
@@ -1,7 +1,5 @@
 package com.kvaster.iptv.m3u;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -11,7 +9,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.zip.GZIPInputStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +18,7 @@ public class M3uParser {
 
     private static final Pattern TAG_PAT = Pattern.compile("#(\\w+)(?:[ :](.*))?");
     private static final Pattern PROP_PAT = Pattern.compile(" *([\\w-_]+)=\"([^\"]*)\"(.*)");
+    private static final Pattern PROP_NONSTD_PAT = Pattern.compile(" *([\\w-_]+)=([^\"][^ ]*)(.*)");
     private static final Pattern INFO_PAT = Pattern.compile("([-+0-9]+) ?(.*),([^,]+)");
 
     public static M3uDoc parse(String content) {
@@ -92,6 +90,9 @@ public class M3uParser {
 
         while (line.length() > 0) {
             Matcher m = PROP_PAT.matcher(line);
+            if (!m.matches()) {
+                m = PROP_NONSTD_PAT.matcher(line);
+            }
             if (m.matches()) {
                 props.put(m.group(1), m.group(2));
                 line = m.group(3).strip();
